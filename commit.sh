b76c1e5dec72fdcc8d326813b503d4dc4b2257a6
Philip Z <philip_chang@163.com>
2022-07-08 15:28:43 +0800

    update doc

--- commit.sh
+++ commit.sh
@@ -1,7 +1,11 @@
 alias git='env https_proxy=http://192.168.50.250:1087 git'
+echo "git pull"
 
-git pull
+env https_proxy=http://192.168.50.250:1087 git pull
 git add -A .
+
+echo "git pull"
+
 git commit -m "update doc"
-git push
+env https_proxy=http://192.168.50.250:1087  git push
 
